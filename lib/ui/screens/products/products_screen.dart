f037aab564435c1a34f066f440979d4ff9be85f5
Ahmed Mohsen <105174620+AhmedMohsen2492@users.noreply.github.com>
2024-07-10 13:13:24 +0300

    Initial commit

---
+++ lib/ui/screens/products/products_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductsScreen extends StatelessWidget {
+  const ProductsScreen({super.key});
+  static const String routeName = "products";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
